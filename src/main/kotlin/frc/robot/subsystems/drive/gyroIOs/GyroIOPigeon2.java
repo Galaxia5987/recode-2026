c7ac39e7e7584b9e5bd62ccb6c9a63903760e384
AdarWa <adar.wasserman@gmail.com>
2026-07-27 21:13:22 +0300

    Migrate Pigeon IO

--- src/main/kotlin/frc/robot/subsystems/drive/gyroIOs/GyroIOPigeon2.java
+++ src/main/kotlin/frc/robot/subsystems/drive/gyroIOs/GyroIOPigeon2.java
@@ -18,16 +18,19 @@ import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.Pigeon2Configuration;
 import com.ctre.phoenix6.hardware.Pigeon2;
-import edu.wpi.first.math.filter.LinearFilter;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.units.measure.Angle;
-import edu.wpi.first.units.measure.AngularVelocity;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.drive.PhoenixOdometryThread;
 import frc.robot.subsystems.drive.TunerConstants;
+import org.wpilib.math.filter.LinearFilter;
+import org.wpilib.math.geometry.Rotation2d;
+import org.wpilib.math.geometry.Translation2d;
+import org.wpilib.math.util.Units;
+import org.wpilib.units.measure.Angle;
+import org.wpilib.units.measure.AngularVelocity;
+
 import java.util.Queue;
+
+import static org.wpilib.units.Units.MetersPerSecondPerSecond;
 
 /** IO implementation for Pigeon 2. */
 public class GyroIOPigeon2 implements GyroIO {
@@ -67,15 +70,15 @@ public class GyroIOPigeon2 implements GyroIO {
         double accelX =
                 pigeon.getAccelerationX()
                         .getValue()
-                        .in(edu.wpi.first.units.Units.MetersPerSecondPerSecond);
+                        .in(MetersPerSecondPerSecond);
         double accelY =
                 pigeon.getAccelerationY()
                         .getValue()
-                        .in(edu.wpi.first.units.Units.MetersPerSecondPerSecond);
+                        .in(MetersPerSecondPerSecond);
         Translation2d accel =
                 new Translation2d(accelX, accelY).rotateBy(Rotation2d.fromDegrees(138.403843 + 90));
-        inputs.accelerationX = edu.wpi.first.units.Units.MetersPerSecondPerSecond.of(accel.getX());
-        inputs.accelerationY = edu.wpi.first.units.Units.MetersPerSecondPerSecond.of(accel.getY());
+        inputs.accelerationX = MetersPerSecondPerSecond.of(accel.getX());
+        inputs.accelerationY = MetersPerSecondPerSecond.of(accel.getY());
         inputs.yawPosition = Rotation2d.fromDegrees(yaw.getValueAsDouble());
         inputs.pitchPosition = Rotation2d.fromDegrees(pitch.getValueAsDouble());
         inputs.rollPosition = Rotation2d.fromDegrees(roll.getValueAsDouble());
