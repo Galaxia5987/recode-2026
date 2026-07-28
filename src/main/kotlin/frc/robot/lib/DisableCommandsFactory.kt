14f6b2ea20a300d43f7449b5709d20caacf3d892
AdarWa <adar.wasserman@gmail.com>
2026-07-28 13:35:06 +0300

    Migrate DisableCommandsFactory.kt to 2027

--- src/main/kotlin/frc/robot/lib/DisableCommandsFactory.kt
+++ src/main/kotlin/frc/robot/lib/DisableCommandsFactory.kt
@@ -1,16 +1,16 @@
 package frc.robot.lib
 
 import com.ctre.phoenix6.controls.CoastOut
-import edu.wpi.first.wpilibj.DriverStation
-import edu.wpi.first.wpilibj2.command.Commands
-import edu.wpi.first.wpilibj2.command.button.Trigger
 import frc.robot.lib.universal_motor.UniversalTalonFX
+import org.wpilib.command3.Command
+import org.wpilib.command3.Trigger
+import org.wpilib.driverstation.RobotState
 
 private val coastOut = CoastOut()
 
 fun createDisableTriggerForCoast(motor: UniversalTalonFX) {
     val coast = { motor.setControl(coastOut) }
-    Trigger { DriverStation.isDisabled() }
-        .onTrue(Commands.runOnce(coast).ignoringDisable(true))
+    Trigger { RobotState.isDisabled() }
+        .onTrue(Command.noRequirements{coast.invoke()}.named("CoastMotor"))
     coast.invoke()
 }
