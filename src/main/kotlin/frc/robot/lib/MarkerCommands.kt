887d3093fc46172eb3c3ee07f4d6767e44ef3c49
AdarWa <adar.wasserman@gmail.com>
2026-07-28 14:06:16 +0300

    Migrate MarkerCommands.kt to 2027

--- src/main/kotlin/frc/robot/lib/MarkerCommands.kt
+++ src/main/kotlin/frc/robot/lib/MarkerCommands.kt
@@ -1,22 +1,21 @@
 package frc.robot.lib
 
-import edu.wpi.first.wpilibj.DataLogManager
-import edu.wpi.first.wpilibj2.command.Command
-import edu.wpi.first.wpilibj2.command.Commands
+import frc.robot.lib.extensions.sec
+import org.wpilib.system.DataLogManager
 import org.littletonrobotics.junction.AutoLogOutput
+import org.wpilib.command3.Command
 
 const val ABNORMAL_EVENT_NAME = "### [Error] DRIVER PROBLEM REPORTED ###"
 
 @AutoLogOutput private var marked = false
 
 private fun markEvent(eventName: String): Command =
-    Commands.sequence(
-        Commands.runOnce({
-            DataLogManager.log(eventName)
-            marked = true
-        }),
-        Commands.waitSeconds(5.0),
-        Commands.runOnce({ marked = false })
-    )
+    Command.noRequirements {
+        DataLogManager.log(eventName)
+        marked = true
+        it.wait(5.0.sec)
+        marked = false
+    }.named("MarkEvent")
+
 
 fun markAbnormalEvent(): Command = markEvent(ABNORMAL_EVENT_NAME)
