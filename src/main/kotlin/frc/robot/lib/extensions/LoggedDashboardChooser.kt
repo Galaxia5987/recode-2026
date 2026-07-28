5e6dc0be67d511f4b4b8c0cdd0780bf9ed3a6b16
AdarWa <adar.wasserman@gmail.com>
2026-07-28 13:50:32 +0300

    Migrate LoggedDashboardChooser.kt to 2027

--- src/main/kotlin/frc/robot/lib/extensions/LoggedDashboardChooser.kt
+++ src/main/kotlin/frc/robot/lib/extensions/LoggedDashboardChooser.kt
@@ -1,6 +1,6 @@
 package frc.robot.lib.extensions
 
-import edu.wpi.first.networktables.NetworkTableInstance
+import org.wpilib.networktables.NetworkTableInstance
 
 // IMPORTANT: USE THIS ONLY IF YOU ALREADY ADDED THE KEY TO THE CHOOSER!!!
 fun setLoggedAutoChooser(chooserName: String, key: String) {
