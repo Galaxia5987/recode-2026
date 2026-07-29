7d454e1daf15e4a51d9760d61fd906c9e9c7fdc7
AdarWa <adar.wasserman@gmail.com>
2026-07-29 14:02:41 +0300

    Add `addPeriodic` extension function

---
+++ src/main/kotlin/frc/robot/lib/commands/MechanismExtensions.kt
@@ -0,0 +1,18 @@
+package frc.robot.lib.commands
+
+import org.littletonrobotics.junction.Logger
+import org.wpilib.command3.Mechanism
+import org.wpilib.command3.Scheduler
+import org.wpilib.system.RobotController
+import org.wpilib.units.Units.Microseconds
+import org.wpilib.units.Units.Millisecond
+
+fun Mechanism.addPeriodic(function: ()->Unit){
+    val timeLogPath = "Subsystems/$name/loopTime"
+    Scheduler.getDefault().addPeriodic {
+        val startTime = RobotController.getTime()
+        function.invoke()
+        val totalTime = Millisecond.convertFrom((RobotController.getTime() - startTime).toDouble(), Microseconds)
+        Logger.recordOutput(timeLogPath, totalTime)
+    }
+}
