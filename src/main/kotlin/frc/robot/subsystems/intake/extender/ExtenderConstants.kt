9416db6acc9cd8853943a48849a355735a1ac969
GitHubDanya <daniel.4ervinsky@gmail.com>
2026-08-09 18:02:25 +0300

    Add closing voltage to constants

--- src/main/kotlin/frc/robot/subsystems/intake/extender/ExtenderConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/extender/ExtenderConstants.kt
@@ -3,6 +3,9 @@ package frc.robot.subsystems.intake.extender
 import com.ctre.phoenix6.configs.TalonFXConfiguration
 import frc.robot.lib.Gains
 import frc.robot.lib.extensions.cm
+import frc.robot.lib.extensions.deg_ps
+import frc.robot.lib.extensions.volts
+import org.wpilib.units.measure.AngularVelocity
 
 val PORT = 1
 
@@ -13,8 +16,12 @@ val GEAR_RATIO = 1 / 0.5
 
 val DIAMETER = 1.0.cm
 
+val CLOSING_MIN_VELOCITY: AngularVelocity = 1.0.deg_ps
+
 val CONFIG = TalonFXConfiguration()
 
 enum class ExtenderState {
     OPENING, CLOSING, IDLE
 }
+
+val CLOSING_VOLTAGE = 1.volts
