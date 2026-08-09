b58efd1265f9c4ae5335618d166c304898c03fba
Daniel Chervinsky <134600927+GitHubDanya@users.noreply.github.com>
2026-08-10 01:46:49 +0300

    Refactor logging
    Co-authored-by: Dan Katzuv <31829093+katzuv@users.noreply.github.com>

--- src/main/kotlin/frc/robot/subsystems/intake/extender/Extender.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/extender/Extender.kt
@@ -2,33 +2,26 @@ package frc.robot.subsystems.intake.extender
 
 import com.ctre.phoenix6.controls.PositionVoltage
 import com.ctre.phoenix6.controls.VoltageOut
-import frc.robot.CURRENT_MODE
-import frc.robot.lib.Mode
 import frc.robot.lib.commands.addPeriodic
 import frc.robot.lib.commands.invoke
 import frc.robot.lib.commands.unaryPlus
-import frc.robot.lib.extensions.deg
 import frc.robot.lib.extensions.deg_ps
 import frc.robot.lib.extensions.get
+import frc.robot.lib.extensions.log
 import frc.robot.lib.extensions.meters
-import frc.robot.lib.extensions.seconds
+import frc.robot.lib.extensions.sec
 import frc.robot.lib.extensions.toAngle
-import frc.robot.lib.math.differential.Derivative
-import frc.robot.lib.universal_motor.LoggedMotorInputs
-import frc.robot.lib.universal_motor.MotorLogConfig
-import frc.robot.lib.universal_motor.UniversalTalonFX
 import org.littletonrobotics.junction.Logger
 import org.wpilib.command3.Command
 import org.wpilib.command3.Mechanism
 import org.wpilib.command3.Trigger
 import org.wpilib.math.filter.LinearFilter
+import org.wpilib.system.Timer
 import org.wpilib.units.measure.AngularVelocity
-import kotlin.math.abs
 
 object Extender : Mechanism()
 {
     private val io = ExtenderIOSim()
-
     private val voltageOut = VoltageOut(0.0)
     private val positionVoltage = PositionVoltage(0.0)
 
@@ -55,7 +48,8 @@ object Extender : Mechanism()
         {
             io.setControl(voltageOut.withOutput(0.0))
         }
-    }.named("Subsystems/Extender/Pump")
+    }.withPriority(Command.LOWEST_PRIORITY)
+        .named("Subsystems/Extender/Pump")
 
     fun open() : Command = this {
         setpoint = OPEN_POSITION
@@ -66,7 +60,8 @@ object Extender : Mechanism()
         )
 
         waitUntil(atSetpoint)
-    }.named("Subsystems/Extender/Open")
+    }.withPriority(Command.DEFAULT_PRIORITY)
+        .named("Subsystems/Extender/Open")
 
     fun close() : Command = this {
         io.setControl(voltageOut.withOutput(CLOSING_VOLTAGE))
@@ -74,10 +69,11 @@ object Extender : Mechanism()
         setpoint = 0.meters
 
         val filter = LinearFilter.movingAverage(5)
-        var velocity = 0.deg_ps
+        var filterVelocity = 0.deg_ps
 
         fun waitWhile(condition: (velocity: AngularVelocity) -> Boolean) {
-            while (true) {
+            val startTimestamp = Timer.getTimestamp()
+            while (Timer.getTimestamp() - startTimestamp < CLOSING_TIMEOUT[sec]) {
                 val currentVelocity = filter.calculate(io.inputs.velocity[deg_ps]).deg_ps
                 Logger.recordOutput("Subsystems/Extender/velocity", currentVelocity)
 
@@ -88,9 +84,9 @@ object Extender : Mechanism()
 
         waitWhile { it >= CLOSING_MIN_VELOCITY }
         waitWhile { it <= CLOSING_MIN_VELOCITY }
-
         +stop()
-    }.named("Subsystems/Extender/Close")
+    }.withPriority(Command.DEFAULT_PRIORITY)
+        .named("Subsystems/Extender/Close")
 
     fun stop() : Command = this {
         io.setControl(voltageOut.withOutput(0.0))
@@ -100,19 +96,11 @@ object Extender : Mechanism()
     fun periodic() {
         io.updateInputs()
 
-        Logger.recordOutput(
-            "Subsystems/Extender/atSetpoint",
-            atSetpoint
-        )
-
-        Logger.recordOutput(
-            "Subsystems/Extender/setpoint",
-            setpoint
-        )
-
-        Logger.recordOutput(
-            "Subsystems/Extender/extenderState",
-            extenderState
-        )
+        mapOf(
+                "atSetpoint" to atSetpoint,
+                "setpoint" to setpoint,
+                "extender" to extenderState,
+            )
+            .forEach { (key, value) -> value.log("Subsystems/Extender", key) }
     }
 }
