5c9a12ae8691669f291f6b689627cf2a962c4f06
yahali <97111125+yakla@users.noreply.github.com>
2026-08-11 16:48:29 +0300

    Change to logger from loggedoutput
    Co-authored-by: joemekh <joemekhan@gmail.com>

--- src/main/kotlin/frc/robot/subsystems/spindexer/Spindexer.kt
+++ src/main/kotlin/frc/robot/subsystems/spindexer/Spindexer.kt
@@ -5,8 +5,7 @@ import frc.robot.lib.commands.addPeriodic
 import frc.robot.lib.commands.invoke
 import frc.robot.lib.extensions.deg_ps
 import frc.robot.lib.universal_motor.UniversalTalonFX
-import org.team5987.annotation.LogLevel
-import org.team5987.annotation.LoggedOutput
+import org.littletonrobotics.junction.Logger
 import org.wpilib.command3.Mechanism
 import org.wpilib.command3.Trigger
 
@@ -14,9 +13,7 @@ object Spindexer : Mechanism(), SpindexerVelocitiesCommandFactory {
     val motor = UniversalTalonFX(1, config = MOTOR_CONFIG, simGains = SIM_GAINS)
     val velocityRequest = VelocityVoltage(0.0)
 
-    @LoggedOutput(LogLevel.DEV) var setpoint = 0.deg_ps
-
-    @LoggedOutput(LogLevel.DEV)
+    var setpoint = 0.deg_ps
     val atSetpoint = Trigger {
         motor.inputs.velocity.isNear(setpoint, SETPOINT_TOLERANCE)
     }
@@ -30,20 +27,11 @@ object Spindexer : Mechanism(), SpindexerVelocitiesCommandFactory {
         motor.setControl(velocityRequest.withVelocity(value.velocity))
         yield()
         waitUntil(atSetpoint)
-
-        //        var i = 0
-        //        println("***********************")
-        //        while (true) {
-        //            yield()
-        //            print("${i++}: ${atSetpoint.asBoolean}, Current:
-        // ${motor.inputs.velocity[rps]}, Setpoint: ${setpoint[rps]}")
-        //            if (atSetpoint.asBoolean) {
-        //                break
-        //            }
-        //        }
     }
 
     private fun periodic() {
         motor.periodic()
+        Logger.recordOutput("subsystem/$name/setpoint",setpoint)
+        Logger.recordOutput("subsystem/$name/atSetpoint",atSetpoint)
     }
 }
--- src/main/kotlin/frc/robot/subsystems/spindexer/SpindexerTest.kt
+++
@@ -1,6 +0,0 @@
-package frc.robot.subsystems.spindexer
-
-// class SpindexerTest: Tests() {
-//    @Test
-//    fun ``
-// }
