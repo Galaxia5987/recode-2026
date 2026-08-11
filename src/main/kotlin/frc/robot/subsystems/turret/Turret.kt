df4d9dd7b884461d3ef3caa74ad8fac42e8506b4
AdarWa <adar.wasserman@gmail.com>
2026-08-11 19:12:33 +0300

    Started writing the turret
    Co-authored-by: GoniShahaf <goni.shahaf@gmail.com>

--- src/main/kotlin/frc/robot/subsystems/turret/Turret.kt
+++ src/main/kotlin/frc/robot/subsystems/turret/Turret.kt
@@ -1,52 +1,36 @@
 package frc.robot.subsystems.turret
 
 import com.ctre.phoenix6.CANBus.systemcore
-import com.ctre.phoenix6.configs.TalonFXConfiguration
 import com.ctre.phoenix6.controls.PositionVoltage
 import com.ctre.phoenix6.hardware.CANcoder
-import frc.robot.lib.Gains
 import frc.robot.lib.commands.invoke
 import frc.robot.lib.extensions.deg
 import frc.robot.lib.universal_motor.UniversalTalonFX
 import org.wpilib.command3.Mechanism
+import org.wpilib.command3.Trigger
 import org.wpilib.units.measure.Angle
 
 object Turret : Mechanism() {
-
-    private val MAX_ANGLE: Angle = 270.0.deg
-    private val MIN_ANGLE: Angle = (-10.0).deg
-    private val absoluteEncoder = CANcoder(5, systemcore(3))
+    private val absoluteEncoder = CANcoder(ENCODER_ID, systemcore(3))
     private val motor: UniversalTalonFX =
         UniversalTalonFX(
             port = PORT,
-            config = TalonFXConfiguration(), // what is this
-            gearRatio = 0.0,
-            simGains = Gains(), // TODO understand
+            config = CONFIG,
+            gearRatio = RATIO,
+            simGains = SIM_GAINS,
         )
 
-    var targetAngle: Angle = 0.deg
+    var setpoint: Angle = 0.deg
+    val positionVoltage = PositionVoltage(0.0)
+    val atSetpoint = Trigger { motor.inputs.position.isNear(setpoint, TOLERANCE) }
 
     init {
         absoluteEncoder.configurator.apply(ENCODER_CONFIG)
     }
 
-    fun setAngle(angle: Angle) {
-        var realAngle = angle
-        if (realAngle > MAX_ANGLE) {
-            realAngle = MAX_ANGLE
-        }
-        if (realAngle < MIN_ANGLE) {
-            realAngle = MIN_ANGLE
-        }
-        targetAngle = angle
-        motor.setControl(PositionVoltage(angle))
-    }
-
-    fun getAngle(): Angle {
-        return motor.inputs.position
-    }
-
-    fun turretLimits() {
-        if (getAngle() < MAX_ANGLE && getAngle() > MIN_ANGLE) {}
+    fun setAngle(angle: Angle) = this {
+        setpoint = angle
+        motor.setControl(positionVoltage.withPosition(setpoint))
+        waitUntil(atSetpoint)
     }
 }
--- src/main/kotlin/frc/robot/subsystems/turret/TurretConstrants.kt
+++ src/main/kotlin/frc/robot/subsystems/turret/TurretConstrants.kt
@@ -1,28 +1,53 @@
 package frc.robot.subsystems.turret
 
-import com.ctre.phoenix6.configs.CANcoderConfiguration
-import com.ctre.phoenix6.configs.TalonFXConfiguration
+import com.ctre.phoenix6.configs.*
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue
+import com.ctre.phoenix6.signals.InvertedValue
+import com.ctre.phoenix6.signals.NeutralModeValue
 import com.ctre.phoenix6.signals.SensorDirectionValue
 import frc.robot.lib.Gains
+import frc.robot.lib.createCurrentLimits
 import frc.robot.lib.extensions.deg
 import frc.robot.lib.extensions.get
-import frc.robot.lib.extensions.rotations
-import org.wpilib.units.measure.Angle
-
-private val MAX_ANGLE: Angle = 270.0.deg
-private val MIN_ANGLE: Angle = (-10.0).deg
+import frc.robot.lib.extensions.rps
 
 val PORT = 1
-val CONFIG = TalonFXConfiguration()
-val GEAR_RATIO = 0.0
-val SIM_GAINS = Gains()
+val SIM_GAINS = Gains(kP = 0.5, kD = 0.075)
+val REAL_GAINS = Gains(kP = 0.5, kD = 0.075 )
+val ABSOLUTE_ENCODER_OFFSET = 0.rps
+const val RATIO = 1.0
+val ENCODER_ID = 0
 
 
-// TODO: FIND PROBLEM
-//val ENCODER_CONFIG =     CANcoderConfiguration().apply {
-//    MagnetSensor.SensorDirection =
-//        SensorDirectionValue.CounterClockwise_Positive
-//    MagnetSensor.AbsoluteSensorDiscontinuityPoint = 0.5
-//    MagnetSensor.MagnetOffset = ABSOLUTE_ENCODER_OFFSET[rotations]
-//}
+val ENCODER_CONFIG =
+    CANcoderConfiguration().apply{
+        MagnetSensor.SensorDirection =
+            SensorDirectionValue.CounterClockwise_Positive
+        MagnetSensor.AbsoluteSensorDiscontinuityPoint = 0.5
+        MagnetSensor.MagnetOffset =
+            ABSOLUTE_ENCODER_OFFSET[rps]
+    }
 
+val CONFIG =
+    TalonFXConfiguration().apply {
+        MotorOutput =
+            MotorOutputConfigs().apply {
+                NeutralMode = NeutralModeValue.Coast
+                Inverted = InvertedValue.Clockwise_Positive
+            }
+        CurrentLimits = createCurrentLimits()
+        Slot0 =
+            Slot0Configs().apply {
+                Slot0 = REAL_GAINS.toSlotConfig()
+            }
+
+
+        Feedback =
+            FeedbackConfigs().apply {
+                SensorToMechanismRatio = RATIO
+                FeedbackRemoteSensorID = ENCODER_ID
+                FeedbackSensorSource = FeedbackSensorSourceValue.FusedCANcoder
+            }
+    }
+
+val TOLERANCE = 1.deg
