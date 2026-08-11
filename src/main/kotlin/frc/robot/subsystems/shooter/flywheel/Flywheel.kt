0a470c5e6867f392b7383d15294ffe231b63e767
GitHubDanya <daniel.4ervinsky@gmail.com>
2026-08-11 16:33:12 +0300

    Import configs for motors

--- src/main/kotlin/frc/robot/subsystems/shooter/flywheel/Flywheel.kt
+++ src/main/kotlin/frc/robot/subsystems/shooter/flywheel/Flywheel.kt
@@ -3,6 +3,7 @@ package frc.robot.subsystems.shooter.flywheel
 import com.ctre.phoenix6.controls.VelocityVoltage
 import frc.robot.lib.extensions.deg_ps
 import frc.robot.lib.extensions.mps
+import frc.robot.lib.universal_motor.MotorLogConfig
 import frc.robot.lib.universal_motor.UniversalTalonFX
 import org.wpilib.command3.Trigger
 import org.wpilib.units.measure.AngularVelocity
@@ -11,13 +12,44 @@ import org.wpilib.units.measure.Velocity
 object Flywheel {
     val motors: Array<UniversalTalonFX> = arrayOf(
         UniversalTalonFX(
-            port = PORT
+            port = MAIN_MOTOR_PORT,
+            config = MOTOR_CONFIG,
+            logConfig =
+                MotorLogConfig(
+                    position = false,
+                    statorCurrent = false,
+                    current = false,
+                    velocity = false,
+                    absoluteEncoder = false,
+                    voltage = true
+                )
         ),
         UniversalTalonFX(
-            port = PORT
+            port = FIRST_AUX_MOTOR_PORT,
+            config = MOTOR_CONFIG,
+            logConfig =
+                MotorLogConfig(
+                    position = false,
+                    statorCurrent = false,
+                    current = false,
+                    velocity = false,
+                    absoluteEncoder = false,
+                    voltage = true
+
+                )
         ),
         UniversalTalonFX(
-            port = PORT
+            port = SECOND_AUX_MOTOR_PORT,
+            config = MOTOR_CONFIG,
+            logConfig =
+                MotorLogConfig(
+                    position = false,
+                    statorCurrent = false,
+                    current = false,
+                    velocity = false,
+                    absoluteEncoder = false,
+                    voltage = true
+                )
         )
     )
 
--- src/main/kotlin/frc/robot/subsystems/shooter/flywheel/FlywheelConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/shooter/flywheel/FlywheelConstants.kt
@@ -1,11 +1,42 @@
 package frc.robot.subsystems.shooter.flywheel
 
+import com.ctre.phoenix6.configs.FeedbackConfigs
+import com.ctre.phoenix6.configs.MotorOutputConfigs
 import com.ctre.phoenix6.configs.TalonFXConfiguration
+import com.ctre.phoenix6.signals.InvertedValue
+import com.ctre.phoenix6.signals.NeutralModeValue
+import frc.robot.lib.Gains
+import frc.robot.lib.createCurrentLimits
+import frc.robot.lib.extensions.amps
 import frc.robot.lib.extensions.deg_ps
 import frc.robot.lib.extensions.mps
 
-const val PORT = 2
+const val MAIN_MOTOR_PORT = 2
+const val FIRST_AUX_MOTOR_PORT = 3
+const val SECOND_AUX_MOTOR_PORT = 4
 
-val CONFIG = TalonFXConfiguration()
+
+const val GEAR_RATIO = 1.33
+val REAL_GAINS = Gains(kP = 0.3, kS = 0.3, kV = 0.115)
+val SIM_GAINS = Gains(kP = 1.0, kV = 0.12)
+
+val REGULAR_CURRENT_LIMITS =
+    createCurrentLimits(
+        supplyCurrentLimit = 30.amps,
+        supplyCurrentPeakDifference = 5.amps
+    )
+
+val MOTOR_CONFIG =
+    TalonFXConfiguration().apply {
+        MotorOutput =
+            MotorOutputConfigs().apply {
+                NeutralMode = NeutralModeValue.Coast
+                Inverted = InvertedValue.Clockwise_Positive
+            }
+        Feedback = FeedbackConfigs().apply { GEAR_RATIO }
+        Slot0 = REAL_GAINS.toSlotConfig()
+
+        CurrentLimits = REGULAR_CURRENT_LIMITS
+    }
 
 val TOLERANCE = 1.deg_ps
