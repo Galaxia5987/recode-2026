19ef610e788c91b2f6f08eb15e3e81bd1698186d
yahali <97111125+yakla@users.noreply.github.com>
2026-08-14 11:57:27 +0300

    Change variables to private

--- src/main/kotlin/frc/robot/subsystems/spindexer/Spindexer.kt
+++ src/main/kotlin/frc/robot/subsystems/spindexer/Spindexer.kt
@@ -3,17 +3,20 @@ package frc.robot.subsystems.spindexer
 import com.ctre.phoenix6.controls.VelocityVoltage
 import frc.robot.lib.commands.addPeriodic
 import frc.robot.lib.commands.invoke
-import frc.robot.lib.extensions.deg_ps
+import frc.robot.lib.commands.waitUntil
+import frc.robot.lib.extensions.rps
 import frc.robot.lib.universal_motor.UniversalTalonFX
 import org.littletonrobotics.junction.Logger
 import org.wpilib.command3.Mechanism
 import org.wpilib.command3.Trigger
 
 object Spindexer : Mechanism(), SpindexerVelocitiesCommandFactory {
-    val motor = UniversalTalonFX(1, config = MOTOR_CONFIG, simGains = SIM_GAINS)
-    val velocityRequest = VelocityVoltage(0.0)
+    private val motor = UniversalTalonFX(PORT, config = MOTOR_CONFIG, simGains = SIM_GAINS)
+    private val velocityRequest = VelocityVoltage(0.0)
 
-    var setpoint = 0.deg_ps
+    private var setpoint = 0.rps
+
+
     val atSetpoint = Trigger {
         motor.inputs.velocity.isNear(setpoint, SETPOINT_TOLERANCE)
     }
