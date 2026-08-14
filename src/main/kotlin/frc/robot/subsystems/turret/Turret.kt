fabfdd26400b138df011dee62e3268812b53f0ea
Paulo Khayat <paulokhayat@gmail.com>
2026-08-14 08:37:42 -0400

    Create angleSupplier command

--- src/main/kotlin/frc/robot/subsystems/turret/Turret.kt
+++ src/main/kotlin/frc/robot/subsystems/turret/Turret.kt
@@ -6,10 +6,13 @@ import com.ctre.phoenix6.hardware.CANcoder
 import frc.robot.lib.commands.addPeriodic
 import frc.robot.lib.commands.invoke
 import frc.robot.lib.extensions.deg
+import frc.robot.lib.extensions.rot
 import frc.robot.lib.universal_motor.UniversalTalonFX
 import org.wpilib.command3.Mechanism
 import org.wpilib.command3.Trigger
+import org.wpilib.system.Timer
 import org.wpilib.units.measure.Angle
+import kotlin.math.sin
 
 object Turret : Mechanism() {
     private val absoluteEncoder = CANcoder(ENCODER_ID, systemcore(3))
@@ -31,6 +34,14 @@ object Turret : Mechanism() {
         setpoint = angle
         motor.setControl(positionVoltage.withPosition(setpoint))
         waitUntil(atSetpoint)
+    }
+
+    fun setAngle(angleSupplier: () -> Angle) = this {
+        while(true){
+            setpoint = angleSupplier.invoke()
+            motor.setControl(positionVoltage.withPosition(setpoint))
+            yield()
+        }
     }
 
     init {
