41fb2bc3b029276ef20bd8539475c694044453f2
asafthedev <asafshotland@gmail.com>
2026-08-14 13:12:35 +0300

    add roller subsystem formating
    Co-authored-by: Dotan-666 <dotan.hillel@gmail.com>
    Co-authored-by: joemekh <joemekhan@gmail.com>

--- src/main/kotlin/frc/robot/subsystems/intake/roller/Roller.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/roller/Roller.kt
@@ -3,24 +3,25 @@ package frc.robot.subsystems.intake.roller
 import com.ctre.phoenix6.controls.Follower
 import com.ctre.phoenix6.controls.VoltageOut
 import com.ctre.phoenix6.signals.MotorAlignmentValue
+import frc.robot.lib.commands.UnnamedCommand
 import frc.robot.lib.commands.invoke
+import frc.robot.lib.extensions.volts
 import frc.robot.lib.universal_motor.UniversalTalonFX
 import org.wpilib.command3.Command
 import org.wpilib.command3.Mechanism
+import org.wpilib.units.measure.Voltage
 
 object Roller : Mechanism() {
-    val mainMotor =
+    private val mainMotor =
         UniversalTalonFX(
             port = MAIN_MOTOR_PORT,
             config = MOTOR_CONFIG,
-            gearRatio = GEAR_RATIO,
             logConfig = MOTOR_LOG_CONFIG,
         )
-    val auxMotor =
+    private val auxMotor =
         UniversalTalonFX(
             port = AUX_MOTOR_PORT,
             config = MOTOR_CONFIG,
-            gearRatio = GEAR_RATIO,
             logConfig = MOTOR_LOG_CONFIG,
         )
 
@@ -32,21 +33,13 @@ object Roller : Mechanism() {
 
     private val voltageOut = VoltageOut(0.0)
 
-    fun inTake(voltageOut: VoltageOut): Command =
-        this {
-                mainMotor.setControl(voltageOut.withOutput(12.0))
-            }
-            .named("Subsystems/Roller/InTake")
+    private fun setVoltage(voltage : Voltage) : UnnamedCommand =
+        this{
+            mainMotor.setControl(voltageOut.withOutput(voltage))
+        }
 
-    fun outTake(voltageOut: VoltageOut): Command =
-        this {
-                mainMotor.setControl(voltageOut.withOutput(-12.0))
-            }
-            .named("Subsystems/Roller/OutTake")
+    fun intake () : Command = setVoltage(CLOCKWISE).named("Subsystems/Roller/intake")
+    fun outtake () : Command = setVoltage(-CLOCKWISE).named("Subsystems/Roller/outtake")
+    fun stop () : Command = setVoltage(0.0.volts).named("Subsystems/Roller/stop")
 
-    fun stop(voltageOut: VoltageOut): Command =
-        this {
-                mainMotor.setControl(voltageOut.withOutput(0.0))
-            }
-            .named("Subsystems/Roller/Stop")
 }
--- src/main/kotlin/frc/robot/subsystems/intake/roller/RollerConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/roller/RollerConstants.kt
@@ -5,11 +5,12 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration
 import com.ctre.phoenix6.signals.InvertedValue
 import frc.robot.lib.createCurrentLimits
 import frc.robot.lib.extensions.amps
+import frc.robot.lib.extensions.volts
 import frc.robot.lib.universal_motor.MotorLogConfig
 
 val MAIN_MOTOR_PORT = 1
 val AUX_MOTOR_PORT = 2
-val GEAR_RATIO = 1.33
+val CLOCKWISE = 12.0.volts
 
 val MOTOR_CONFIG =
     TalonFXConfiguration().apply {
