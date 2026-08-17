9cbca5465959f4bca8d463599895219c3a107946
GitHubDanya <daniel.4ervinsky@gmail.com>
2026-08-17 23:22:43 +0300

    Initial commit

---
+++ src/main/kotlin/frc/robot/setpoint_manager/SetpointManager.kt
@@ -0,0 +1,23 @@
+package frc.robot.setpoint_manager
+
+import frc.robot.lib.extensions.deg
+import frc.robot.lib.extensions.degrees
+import frc.robot.lib.extensions.periodic
+import org.wpilib.units.measure.Angle
+
+object SetpointManager {
+    fun getTurretSetpoint(): Angle {
+        // TODO
+        return 0.degrees
+    }
+
+    fun getHoodSetpoint(): Angle {
+        // TODO
+        return 0.degrees
+    }
+
+    fun getFlywheelSetpoint(): Angle {
+        // TODO
+        return 0.degrees
+    }
+}
