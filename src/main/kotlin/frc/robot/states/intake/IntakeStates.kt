35c2fce9a300b6fb5d9f03ef092a1f7dbd370aae
asafthedev <asafshotland@gmail.com>
2026-08-20 17:00:42 +0300

    add intake state machine
    Co-authored-by: joemekh <joemekhan@gmail.com>

---
+++ src/main/kotlin/frc/robot/states/intake/IntakeStates.kt
@@ -0,0 +1,24 @@
+package frc.robot.states.intake
+
+import frc.robot.lib.commands.UnnamedCommand
+import frc.robot.lib.commands.command
+import org.wpilib.command3.Command
+import org.wpilib.command3.Trigger
+import frc.robot.lib.commands.invoke
+import frc.robot.subsystems.drive.DriveCommands
+
+
+enum class IntakeState{
+    IDLE,
+    PUMPING,
+    INTAKING,
+    OUTTAKING;
+
+    val trigger =  Trigger { state == this }
+
+    fun set(): Command = command {
+        state = this@IntakeState
+    }.named("IntakeStates/Set")
+}
+
+private var state: IntakeState = IntakeState.IDLE
