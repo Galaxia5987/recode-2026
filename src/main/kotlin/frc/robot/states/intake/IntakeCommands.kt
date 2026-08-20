0121e3c7a294934b9b666f4a91528f44c56b6ab2
asafthedev <asafshotland@gmail.com>
2026-08-20 17:05:35 +0300

    add intake state machine
    Co-authored-by: joemekh <joemekhan@gmail.com>

--- src/main/kotlin/frc/robot/states/intake/IntakeCommands.kt
+++ src/main/kotlin/frc/robot/states/intake/IntakeCommands.kt
@@ -1,23 +1,31 @@
 package frc.robot.states.intake
+
 import frc.robot.lib.commands.command
+import frc.robot.lib.commands.unaryPlus
 import frc.robot.subsystems.intake.extender.Extender
 import frc.robot.subsystems.intake.roller.Roller
 import org.wpilib.command3.Command
-import frc.robot.lib.commands.unaryPlus
 
-fun idle(): Command = command{
-    +[Roller.stop(), Extender.close()]
-}.named("IntakeCommands/Idle")
+fun idle(): Command =
+    command {
+            +[Roller.stop(), Extender.close()]
+        }
+        .named("IntakeCommands/Idle")
 
-fun pumping(): Command = command{
-    +[Roller.intake(), Extender.pump()]
-}.named("IntakeCommands/Pumping")
+fun pumping(): Command =
+    command {
+            +[Roller.intake(), Extender.pump()]
+        }
+        .named("IntakeCommands/Pumping")
 
-fun intaking(): Command = command{
-    +[Roller.intake(), Extender.open()]
-}.named("IntakeCommands/Intaking")
+fun intaking(): Command =
+    command {
+            +[Roller.intake(), Extender.open()]
+        }
+        .named("IntakeCommands/Intaking")
 
-fun outtaking(): Command = command{
-    +[Roller.outtake(), Extender.open()]
-}.named("IntakeCommands/Outtaking")
-
+fun outtaking(): Command =
+    command {
+            +[Roller.outtake(), Extender.open()]
+        }
+        .named("IntakeCommands/Outtaking")
--- src/main/kotlin/frc/robot/states/intake/IntakeStates.kt
+++ src/main/kotlin/frc/robot/states/intake/IntakeStates.kt
@@ -1,24 +1,23 @@
 package frc.robot.states.intake
 
-import frc.robot.lib.commands.UnnamedCommand
 import frc.robot.lib.commands.command
+import frc.robot.lib.commands.invoke
 import org.wpilib.command3.Command
 import org.wpilib.command3.Trigger
-import frc.robot.lib.commands.invoke
-import frc.robot.subsystems.drive.DriveCommands
 
-
-enum class IntakeState{
+enum class IntakeState {
     IDLE,
     PUMPING,
     INTAKING,
     OUTTAKING;
 
-    val trigger =  Trigger { state == this }
+    val trigger = Trigger { state == this }
 
-    fun set(): Command = command {
-        state = this@IntakeState
-    }.named("IntakeStates/Set")
+    fun set(): Command =
+        command {
+                state = this@IntakeState
+            }
+            .named("IntakeStates/Set")
 }
 
 private var state: IntakeState = IntakeState.IDLE
