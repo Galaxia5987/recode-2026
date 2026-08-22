734250aae829408cfafcf81415aac5d300290672
GitHubDanya <daniel.4ervinsky@gmail.com>
2026-08-22 22:01:22 +0300

    Add shooting target enum

--- src/main/kotlin/frc/robot/setpoint_manager/SetpointCalculator.kt
+++ src/main/kotlin/frc/robot/setpoint_manager/SetpointCalculator.kt
@@ -1,10 +1,15 @@
 package frc.robot.setpoint_manager
 
+import frc.robot.field.DEPOT_TRANSLATION
+import frc.robot.field.HUB_TRANSLATION
+import frc.robot.field.OUTPOST_LOCATION
 import frc.robot.lib.extensions.deg
 import frc.robot.lib.extensions.get
+import frc.robot.lib.extensions.toPose
 import frc.robot.setpoint_manager.calculators.CalibrationSetpointCalculator
 import frc.robot.setpoint_manager.calculators.FeedingSetpointCalculator
 import frc.robot.setpoint_manager.calculators.ShootingSetpointCalculator
+import org.wpilib.math.geometry.Pose2d
 import kotlin.math.abs
 import org.wpilib.math.geometry.Translation2d
 import org.wpilib.units.measure.Angle
@@ -33,6 +38,13 @@ enum class SetpointCalculatorType(val calculator: SetpointCalculator) {
     SHOOTING(ShootingSetpointCalculator),
     FEEDING(FeedingSetpointCalculator),
     CALIBRATION(CalibrationSetpointCalculator)
+}
+
+enum class ShootingTarget(val pose: Pose2d)
+{
+    HUB(HUB_TRANSLATION.toPose()),
+    DEPOT(DEPOT_TRANSLATION.toPose()),
+    OUTPOST(OUTPOST_LOCATION.toPose())
 }
 
 // This function checks if compensating for speed would trigger a large turn of
