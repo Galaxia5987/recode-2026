210ebe5099c4c8d89fa2e20c0fc395e4a16cffbc
GitHubDanya <daniel.4ervinsky@gmail.com>
2026-08-22 15:28:48 +0300

    Add FeedingSetpointCalculator

--- src/main/kotlin/frc/robot/setpoint_manager/SetpointCalculator.kt
+++ src/main/kotlin/frc/robot/setpoint_manager/SetpointCalculator.kt
@@ -1,5 +1,8 @@
 package frc.robot.setpoint_manager
 
+import frc.robot.FeedingShotCalculator.calculateFeedingPitch
+import frc.robot.FeedingShotCalculator.calculateFeedingVelocity
+import frc.robot.FeedingShotCalculator.calculateFeedingYaw
 import frc.robot.ShotCalculator.calculateAngularVelocity
 import frc.robot.ShotCalculator.calculatePitch
 import frc.robot.ShotCalculator.calculateVelocity
@@ -33,6 +36,13 @@ interface SetpointCalculator {
     ): AngularVelocity
 }
 
+// This function checks if compensating for speed would trigger a large turn of the turret.
+fun Angle.accountForOvercompensation(angleToGoal: Angle): Angle {
+    if (abs(this[deg] - angleToGoal[deg]) > 180)
+        return angleToGoal
+    return this
+}
+
 class GenericSetpointCalculator : SetpointCalculator {
     private var kStaticCalibration = 0.97
     private var kMovingCalibration = 0.05
@@ -43,7 +53,7 @@ class GenericSetpointCalculator : SetpointCalculator {
         distanceToGoal: Distance,
     ): Angle {
 
-        val compensatedShootingGoal =
+        val shootingGoal =
             angleToGoal -
                 calculateYaw(
                         distanceToGoal[m],
@@ -52,9 +62,8 @@ class GenericSetpointCalculator : SetpointCalculator {
                     )
                     .deg
 
-        if (abs(compensatedShootingGoal[deg] - angleToGoal[deg]) > 180)
-            return angleToGoal
-        return compensatedShootingGoal
+        // todo: constrain angleToGoal once turret class is implemented
+        return shootingGoal.accountForOvercompensation(angleToGoal)
     }
 
     override fun calculateHoodSetpoint(
@@ -83,5 +92,54 @@ class GenericSetpointCalculator : SetpointCalculator {
                     )
                 ))
             .rps
+    }
+}
+
+class FeedingSetpointCalculator : SetpointCalculator {
+    private val MAX_FEED_VELOCITY_RPS = 42.rps
+
+    override fun calculateTurretSetpoint(
+        speeds: Translation2d,
+        angleToGoal: Angle,
+        distanceToGoal: Distance,
+    ): Angle {
+        val compensatedFeedingGoal =
+                angleToGoal -
+                        calculateFeedingYaw(
+                            distanceToGoal[m],
+                            speeds.x,
+                            speeds.y,
+                        )
+                            .deg
+
+        // todo: constrain angleToGoal once turret class is implemented
+        return compensatedFeedingGoal.accountForOvercompensation(angleToGoal)
+    }
+
+    override fun calculateHoodSetpoint(
+        speeds: Translation2d,
+        distanceToGoal: Distance,
+    ): Angle {
+        return (90.deg - calculateFeedingPitch().deg)
+    }
+
+    override fun calculateFlywheelSetpoint(
+        speeds: Translation2d,
+        distanceToGoal: Distance,
+    ): AngularVelocity {
+        val result =
+            calculateAngularVelocity(
+                calculateFeedingVelocity(
+                    distanceToGoal[m],
+                    speeds.x,
+                    speeds.y,
+                )
+            ).rps
+
+        return if (result > MAX_FEED_VELOCITY_RPS) {
+            MAX_FEED_VELOCITY_RPS
+        } else {
+            result
+        }
     }
 }
