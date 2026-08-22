b3fd2ccd3b69f54485041ba6acff13255ec6d5ba
GitHubDanya <daniel.4ervinsky@gmail.com>
2026-08-22 22:01:48 +0300

    Add triggers for changing shooting targets

--- src/main/kotlin/frc/robot/setpoint_manager/SetpointManager.kt
+++ src/main/kotlin/frc/robot/setpoint_manager/SetpointManager.kt
@@ -1,14 +1,26 @@
 package frc.robot.setpoint_manager
 
 import frc.robot.drive
+import frc.robot.field.DEPOT_TRANSLATION
 import frc.robot.field.HUB_TRANSLATION
+import frc.robot.field.inClimbRectangle
+import frc.robot.field.inExtendedAllianceZone
+import frc.robot.field.isCloserToDepotSide
+import frc.robot.isAuto
 import frc.robot.lib.ToTranslation2d
+import frc.robot.lib.commands.command
 import frc.robot.lib.extensions.CacheManager
 import frc.robot.lib.extensions.log
 import frc.robot.lib.extensions.m
+import frc.robot.lib.extensions.not
+import frc.robot.lib.extensions.onTrue
 import frc.robot.lib.extensions.periodic
 import frc.robot.lib.extensions.rotationToPoint
 import frc.robot.lib.extensions.toPose
+import frc.robot.setpoint_manager.SetpointManager.currentGoal
+import frc.robot.setpoint_manager.calculators.ShootingSetpointCalculator
+import org.team5987.annotation.LogLevel
+import org.team5987.annotation.LoggedOutput
 import org.wpilib.math.geometry.Pose2d
 import org.wpilib.math.geometry.Rotation2d
 import org.wpilib.math.geometry.Translation2d
@@ -16,42 +28,63 @@ import org.wpilib.units.measure.Angle
 import org.wpilib.units.measure.AngularVelocity
 import org.wpilib.units.measure.Distance
 
+// TODO: MOVE ALL OF THE DEFINITIONS BELOW TO THE TURRET SUBSYSTEM
+
+@LoggedOutput(path = "SetpointManager", level = LogLevel.COMP)
+val turretOrientedChassisSpeeds: Translation2d by periodic {
+    // todo: replace with full tangential velocity logic once turret class
+    // is implemented
+    drive.chassisSpeeds.ToTranslation2d()
+}
+
+@LoggedOutput(path = "SetpointManager", level = LogLevel.COMP)
+val turretTranslation: Translation2d by periodic {
+    val rotatedTurretOffset: Translation2d = drive.pose.translation
+    // todo: switch to TURRET_TO_ROBOT.rotateBy(drive.pose.rotation)
+    drive.pose.translation.plus(rotatedTurretOffset)
+}
+
+@LoggedOutput(path = "SetpointManager", level = LogLevel.COMP)
+private val compensatedTurretTranslation: Translation2d by periodic {
+    val rotatedTurretOffset: Translation2d = drive.pose.translation
+    drive.compensatedPose.translation.plus(rotatedTurretOffset)
+}
+
+@LoggedOutput(path = "SetpointManager", level = LogLevel.COMP)
+private val angleToGoal: Rotation2d by periodic {
+    turretTranslation.rotationToPoint(currentGoal.translation)
+}
+
+@LoggedOutput(path = "SetpointManager", level = LogLevel.COMP)
+val turretRotationToGoal: Angle by periodic {
+    (drive.pose.rotation - angleToGoal).measure
+}
+
+@LoggedOutput(path = "SetpointManager", level = LogLevel.COMP)
+val turretDistanceFromGoal: Distance by periodic {
+    turretTranslation.getDistance(currentGoal.translation).m
+}
+
+@LoggedOutput(path = "SetpointManager", level = LogLevel.COMP)
+val compensatedTurretDistanceFromGoal: Distance by periodic {
+    compensatedTurretTranslation.getDistance(currentGoal.translation).m
+}
+
+// TODO: MOVE ALL OF THE DEFINTIONS ABOVE TO THE TURRET SUBSYSTEM
+
 object SetpointManager {
-    private val currentGoal: Pose2d by periodic { HUB_TRANSLATION.toPose() }
-    private val calculator: SetpointCalculator = GenericSetpointCalculator()
+    val currentGoal: Pose2d by periodic { shootingTarget.pose }
+    val calculator: SetpointCalculator by periodic { setpointCalculatorType.calculator }
+    var setpointCalculatorType = SetpointCalculatorType.SHOOTING
+    var shootingTarget: ShootingTarget = ShootingTarget.HUB
+        set(value) {
+            field = value
 
-    val turretOrientedChassisSpeeds: Translation2d by periodic {
-        // todo: replace with full tangential velocity logic once turret class
-        // is implemented
-        drive.chassisSpeeds.ToTranslation2d()
-    }
-
-    private val turretTranslation: Translation2d by periodic {
-        val rotatedTurretOffset: Translation2d = drive.pose.translation
-        // todo: switch to TURRET_TO_ROBOT.rotateBy(drive.pose.rotation)
-        drive.pose.translation.plus(rotatedTurretOffset)
-    }
-
-    private val compensatedTurretTranslation: Translation2d by periodic {
-        val rotatedTurretOffset: Translation2d = drive.pose.translation
-        drive.compensatedPose.translation.plus(rotatedTurretOffset)
-    }
-
-    private val angleToGoal: Rotation2d by periodic {
-        turretTranslation.rotationToPoint(currentGoal.translation)
-    }
-
-    val turretRotationToGoal: Angle by periodic {
-        (drive.pose.rotation - angleToGoal).measure
-    }
-
-    val turretDistanceFromGoal: Distance by periodic {
-        turretTranslation.getDistance(currentGoal.translation).m
-    }
-
-    val compensatedTurretDistanceFromGoal: Distance by periodic {
-        compensatedTurretTranslation.getDistance(currentGoal.translation).m
-    }
+            setpointCalculatorType = when (value) {
+                ShootingTarget.HUB -> SetpointCalculatorType.SHOOTING
+                else -> SetpointCalculatorType.FEEDING
+            }
+        }
 
     val turretSetpoint: Angle by periodic {
         calculator.calculateTurretSetpoint(
@@ -75,16 +108,35 @@ object SetpointManager {
         )
     }
 
-    fun periodic() {
-        CacheManager.invalidateAll()
-
-        mapOf(
-                "angleToGoal" to angleToGoal,
-                "turretDistanceFromGoal" to turretDistanceFromGoal,
-                "turretRotationToGoal" to turretRotationToGoal,
-                "compensatedTurretDistanceFromGoal" to
-                    compensatedTurretDistanceFromGoal,
+    private val goalHubTrigger =
+        inExtendedAllianceZone
+            .or(isAuto)
+            .onTrue(
+                command {
+                    shootingTarget = ShootingTarget.HUB
+                }.named("SetpointManager/targetHub")
             )
-            .log("SetpointManager")
-    }
+
+    private val goalDepotTrigger =
+        isCloserToDepotSide
+            .and(!inExtendedAllianceZone)
+            .and(inClimbRectangle.negate())
+            .and(isAuto.negate())
+            .onTrue(
+                command {
+                    shootingTarget = ShootingTarget.DEPOT
+                }.named("SetpointManager/targetDepot")
+            )
+
+    private val goalOutpostTrigger =
+        isCloserToDepotSide
+            .negate()
+            .and(!inExtendedAllianceZone)
+            .and(inClimbRectangle.negate())
+            .and(isAuto.negate())
+            .onTrue(
+                command {
+                    shootingTarget = ShootingTarget.OUTPOST
+                }.named("SetpointManager/targetOutpost")
+            )
 }
