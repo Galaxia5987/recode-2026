2b98f5ddefd692c1fdcd2a6594082ae94f3303a6
GitHubDanya <daniel.4ervinsky@gmail.com>
2026-08-22 23:08:08 +0300

    Rename logs

--- src/main/kotlin/frc/robot/subsystems/turret/TurretKinematics.kt
+++ src/main/kotlin/frc/robot/subsystems/turret/TurretKinematics.kt
@@ -13,42 +13,42 @@ import org.wpilib.math.geometry.Translation2d
 import org.wpilib.units.measure.Angle
 import org.wpilib.units.measure.Distance
 
-@LoggedOutput(path = "SetpointManager", level = LogLevel.COMP)
+@LoggedOutput(path = "TurretKinematics", level = LogLevel.COMP)
 val turretOrientedChassisSpeeds: Translation2d by periodic {
     // todo: replace with full tangential velocity logic once turret class
     // is implemented
     drive.chassisSpeeds.toTranslation2d()
 }
 
-@LoggedOutput(path = "SetpointManager", level = LogLevel.COMP)
+@LoggedOutput(path = "TurretKinematics", level = LogLevel.COMP)
 val turretTranslation: Translation2d by periodic {
     val rotatedTurretOffset: Translation2d = drive.pose.translation
     // todo: switch to TURRET_TO_ROBOT.rotateBy(drive.pose.rotation)
     drive.pose.translation.plus(rotatedTurretOffset)
 }
 
-@LoggedOutput(path = "SetpointManager", level = LogLevel.COMP)
+@LoggedOutput(path = "TurretKinematics", level = LogLevel.COMP)
 private val compensatedTurretTranslation: Translation2d by periodic {
     val rotatedTurretOffset: Translation2d = drive.pose.translation
     drive.compensatedPose.translation.plus(rotatedTurretOffset)
 }
 
-@LoggedOutput(path = "SetpointManager", level = LogLevel.COMP)
+@LoggedOutput(path = "TurretKinematics", level = LogLevel.COMP)
 private val angleToGoal: Rotation2d by periodic {
     turretTranslation.rotationToPoint(currentGoal.translation)
 }
 
-@LoggedOutput(path = "SetpointManager", level = LogLevel.COMP)
+@LoggedOutput(path = "TurretKinematics", level = LogLevel.COMP)
 val turretRotationToGoal: Angle by periodic {
     (drive.pose.rotation - angleToGoal).measure
 }
 
-@LoggedOutput(path = "SetpointManager", level = LogLevel.COMP)
+@LoggedOutput(path = "TurretKinematics", level = LogLevel.COMP)
 val turretDistanceFromGoal: Distance by periodic {
     turretTranslation.getDistance(currentGoal.translation).m
 }
 
-@LoggedOutput(path = "SetpointManager", level = LogLevel.COMP)
+@LoggedOutput(path = "TurretKinematics", level = LogLevel.COMP)
 val compensatedTurretDistanceFromGoal: Distance by periodic {
     compensatedTurretTranslation.getDistance(currentGoal.translation).m
 }
