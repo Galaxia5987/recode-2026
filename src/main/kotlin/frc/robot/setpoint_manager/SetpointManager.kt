ebf61c561b72532b8ac22d9dbfdb1b263056dde8
GitHubDanya <daniel.4ervinsky@gmail.com>
2026-08-22 23:03:01 +0300

    Move kinematic calculations to TurretKinematics.kt

--- src/main/kotlin/frc/robot/setpoint_manager/SetpointManager.kt
+++ src/main/kotlin/frc/robot/setpoint_manager/SetpointManager.kt
@@ -1,73 +1,22 @@
 package frc.robot.setpoint_manager
 
-import frc.robot.drive
 import frc.robot.field.inClimbRectangle
 import frc.robot.field.inExtendedAllianceZone
 import frc.robot.field.isCloserToDepotSide
 import frc.robot.isAuto
-import frc.robot.lib.toTranslation2d
 import frc.robot.lib.commands.command
-import frc.robot.lib.extensions.m
 import frc.robot.lib.extensions.not
 import frc.robot.lib.extensions.periodic
-import frc.robot.lib.extensions.rotationToPoint
-import frc.robot.setpoint_manager.SetpointManager.currentGoal
-import org.team5987.annotation.LogLevel
-import org.team5987.annotation.LoggedOutput
+import frc.robot.subsystems.turret.compensatedTurretDistanceFromGoal
+import frc.robot.subsystems.turret.turretOrientedChassisSpeeds
+import frc.robot.subsystems.turret.turretRotationToGoal
 import org.wpilib.math.geometry.Pose2d
-import org.wpilib.math.geometry.Rotation2d
-import org.wpilib.math.geometry.Translation2d
 import org.wpilib.units.measure.Angle
 import org.wpilib.units.measure.AngularVelocity
-import org.wpilib.units.measure.Distance
-
-// TODO: MOVE ALL OF THE DEFINITIONS BELOW TO THE TURRET SUBSYSTEM
-
-@LoggedOutput(path = "SetpointManager", level = LogLevel.COMP)
-val turretOrientedChassisSpeeds: Translation2d by periodic {
-    // todo: replace with full tangential velocity logic once turret class
-    // is implemented
-    drive.chassisSpeeds.toTranslation2d()
-}
-
-@LoggedOutput(path = "SetpointManager", level = LogLevel.COMP)
-val turretTranslation: Translation2d by periodic {
-    val rotatedTurretOffset: Translation2d = drive.pose.translation
-    // todo: switch to TURRET_TO_ROBOT.rotateBy(drive.pose.rotation)
-    drive.pose.translation.plus(rotatedTurretOffset)
-}
-
-@LoggedOutput(path = "SetpointManager", level = LogLevel.COMP)
-private val compensatedTurretTranslation: Translation2d by periodic {
-    val rotatedTurretOffset: Translation2d = drive.pose.translation
-    drive.compensatedPose.translation.plus(rotatedTurretOffset)
-}
-
-@LoggedOutput(path = "SetpointManager", level = LogLevel.COMP)
-private val angleToGoal: Rotation2d by periodic {
-    turretTranslation.rotationToPoint(currentGoal.translation)
-}
-
-@LoggedOutput(path = "SetpointManager", level = LogLevel.COMP)
-val turretRotationToGoal: Angle by periodic {
-    (drive.pose.rotation - angleToGoal).measure
-}
-
-@LoggedOutput(path = "SetpointManager", level = LogLevel.COMP)
-val turretDistanceFromGoal: Distance by periodic {
-    turretTranslation.getDistance(currentGoal.translation).m
-}
-
-@LoggedOutput(path = "SetpointManager", level = LogLevel.COMP)
-val compensatedTurretDistanceFromGoal: Distance by periodic {
-    compensatedTurretTranslation.getDistance(currentGoal.translation).m
-}
-
-// TODO: MOVE ALL OF THE DEFINTIONS ABOVE TO THE TURRET SUBSYSTEM
 
 object SetpointManager {
-    private val currentGoal: Pose2d by periodic { shootingTarget.pose }
-    private val calculator: SetpointCalculator by periodic {
+    val currentGoal: Pose2d by periodic { shootingTarget.pose }
+    val calculator: SetpointCalculator by periodic {
         setpointCalculatorType.calculator
     }
     var setpointCalculatorType = SetpointCalculatorType.SHOOTING
