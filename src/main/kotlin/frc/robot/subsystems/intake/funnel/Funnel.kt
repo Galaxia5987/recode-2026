57e9b81eb75b2e8cff6052dafb8153e54da710d6
AdarWa <adar.wasserman@egmail.com>
2026-08-24 16:49:32 +0300

    Add `with` infix operator for control requests

--- src/main/kotlin/frc/robot/subsystems/intake/funnel/Funnel.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/funnel/Funnel.kt
@@ -4,6 +4,7 @@ import com.ctre.phoenix6.controls.VoltageOut
 import frc.robot.lib.commands.UnnamedCommand
 import frc.robot.lib.commands.addPeriodic
 import frc.robot.lib.commands.invoke
+import frc.robot.lib.extensions.with
 import frc.robot.lib.universal_motor.UniversalTalonFX
 import org.wpilib.command3.Mechanism
 
@@ -22,6 +23,6 @@ object Funnel : Mechanism(), FunnelStateCommandFactory {
     private val voltageOut = VoltageOut(0.0)
 
     override fun setTarget(value: FunnelState): UnnamedCommand = this {
-        mainMotor.setControl(voltageOut.withOutput(value.voltage))
+        mainMotor.setControl(voltageOut with value.voltage)
     }
 }
