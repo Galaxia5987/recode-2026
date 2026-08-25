e8ae96cdc2c8fac310928bec6bb6417f350b62b0
nogibogi222 <bordnoga@gmail.com>
2026-08-25 18:38:23 +0300

    add is ready to shoot trigger

--- src/main/kotlin/frc/robot/stateMachine/shooting.kt
+++ src/main/kotlin/frc/robot/stateMachine/shooting.kt
@@ -1,6 +1,12 @@
 package frc.robot.stateMachine
 
+import frc.robot.subsystems.preShooter.PreShooter
+import frc.robot.subsystems.shooter.flywheel.Flywheel
+import frc.robot.subsystems.spindexer.Spindexer
 import org.wpilib.command3.Command
 import org.wpilib.command3.Trigger
 
 var shouldShoot = true
+
+val isReadyToShoot = Flywheel.atSetpoint.and(PreShooter.atSetpoint).and(Spindexer.atSetpoint)
+
