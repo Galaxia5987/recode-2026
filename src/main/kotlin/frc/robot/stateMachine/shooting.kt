b9dbba0eb5d34cae35823dc61756651a37806091
nogibogi222 <bordnoga@gmail.com>
2026-08-25 18:57:27 +0300

    add shoot function

--- src/main/kotlin/frc/robot/stateMachine/shooting.kt
+++ src/main/kotlin/frc/robot/stateMachine/shooting.kt
@@ -1,5 +1,8 @@
 package frc.robot.stateMachine
 
+import frc.robot.lib.commands.command
+import frc.robot.setpoint_manager.SetpointCalculator
+import frc.robot.setpoint_manager.SetpointManager
 import frc.robot.subsystems.preShooter.PreShooter
 import frc.robot.subsystems.shooter.flywheel.Flywheel
 import frc.robot.subsystems.spindexer.Spindexer
@@ -8,5 +11,25 @@ import org.wpilib.command3.Trigger
 
 var shouldShoot = true
 
-val isReadyToShoot = Flywheel.atSetpoint.and(PreShooter.atSetpoint).and(Spindexer.atSetpoint)
+val isReadyToShoot = Flywheel.atSetpoint.and(PreShooter.atSetpoint)
 
+fun shoot() : Command =
+    command {
+        while(true) {
+            while(shouldShoot) {
+                while (isReadyToShoot.negate().asBoolean) {
+                    Flywheel.setVelocity(SetpointManager.flywheelSetpoint)
+                    PreShooter.convey()
+                    yield()
+                }
+                while(isReadyToShoot.asBoolean) {
+                    Flywheel.setVelocity(SetpointManager.flywheelSetpoint)
+                    PreShooter.convey()
+                    Spindexer.convey()
+                    yield()
+                }
+                yield()
+            }
+            yield()
+        }
+    }.named("StateMachine/Shooting/shoot")
