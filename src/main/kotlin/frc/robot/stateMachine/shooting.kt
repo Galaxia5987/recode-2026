8f99fa6a54a2f4952670869644e91abfab8443fe
nogibogi222 <bordnoga@gmail.com>
2026-09-01 15:56:09 +0300

    fix is ready to shoot trigger

--- src/main/kotlin/frc/robot/stateMachine/shooting.kt
+++ src/main/kotlin/frc/robot/stateMachine/shooting.kt
@@ -3,15 +3,17 @@ package frc.robot.stateMachine
 import frc.robot.lib.commands.command
 import frc.robot.setpoint_manager.SetpointCalculator
 import frc.robot.setpoint_manager.SetpointManager
+import frc.robot.subsystems.hood.Hood
 import frc.robot.subsystems.preShooter.PreShooter
 import frc.robot.subsystems.shooter.flywheel.Flywheel
 import frc.robot.subsystems.spindexer.Spindexer
+import frc.robot.subsystems.turret.Turret
 import org.wpilib.command3.Command
 import org.wpilib.command3.Trigger
 
 var shouldShoot = true
 
-val isReadyToShoot = Flywheel.atSetpoint.and(PreShooter.atSetpoint)
+val isReadyToShoot = Flywheel.atSetpoint.and(Turret.atSetpoint).and(Hood.atSetpoint)
 
 fun shoot() : Command =
     command {
